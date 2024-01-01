d4879c819c07dd316365b9d168245c1100898e72
CRAMINH <catia.raminhos@daimler.com>
2024-01-01 21:56:46 +0000

    Add livereload to webpack config

--- 8.2 Course Project_CSSLoaders/webpack.config.js
+++ 8.2 Course Project_CSSLoaders/webpack.config.js
@@ -1,25 +1,43 @@
-const path = require("path");
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    index: "./src/index.js",
-    courses: "./src/pages/courses.js",
+    index: './src/index.js',
+    courses: './src/pages/courses.js'
   },
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  devServer: {
+    static: './dist'
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.s[ac]ss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
   },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html',
+      chunks: ['index'],
+      inject: true
+    }),
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/pages/courses.html'),
+      filename: 'courses.html',
+      chunks: ['courses'],
+      inject: true
+    })
+  ]
 };
