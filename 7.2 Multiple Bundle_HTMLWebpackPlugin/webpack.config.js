db2e538173eb7234bea7fa515fa5a236d7122a2a
CRAMINH <catia.raminhos@daimler.com>
2024-01-01 20:48:34 +0000

    Add livereload to webpack config

--- 7.2 Multiple Bundle_HTMLWebpackPlugin/webpack.config.js
+++ 7.2 Multiple Bundle_HTMLWebpackPlugin/webpack.config.js
@@ -1,27 +1,30 @@
-const path = require("path");
-const htmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    index: "./src/index.js",
-    product: "./src/products.js",
+    index: './src/index.js',
+    product: './src/products.js'
   },
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: './dist'
   },
   plugins: [
     new htmlWebpackPlugin({
-      template: path.resolve(__dirname, "src/index.html"),
-      chunks: ["index"],
+      template: path.resolve(__dirname, 'src/index.html'),
+      chunks: ['index'],
       inject: true,
-      filename: "index.html",
+      filename: 'index.html'
     }),
     new htmlWebpackPlugin({
-      template: path.resolve(__dirname, "src/products.html"),
-      chunks: ["product"],
+      template: path.resolve(__dirname, 'src/products.html'),
+      chunks: ['product'],
       inject: true,
-      filename: "products.html",
-    }),
-  ],
+      filename: 'products.html'
+    })
+  ]
 };
