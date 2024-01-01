2aa7bd553d60040605986a242f19ecfa4405b990
CRAMINH <catia.raminhos@daimler.com>
2024-01-01 20:37:19 +0000

    Inject script tag using HTML Webpack plugin

--- 7.1 Multiple Bundle_MultipleEntry/webpack.config.js
+++ 7.1 Multiple Bundle_MultipleEntry/webpack.config.js
@@ -1,12 +1,27 @@
-const path = require("path");
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    index: "./src/index.js",
-    product: "./src/products.js",
+    index: './src/index.js',
+    product: './src/products.js'
   },
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html',
+      chunks: ['index'],
+      inject: true
+    }),
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/products.html'),
+      filename: 'products.html',
+      chunks: ['product'],
+      inject: true
+    })
+  ]
 };
