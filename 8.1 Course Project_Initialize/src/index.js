74d074e6fa5ba16736687e6c0f4f8f638d746bbb
CRAMINH <catia.raminhos@daimler.com>
2024-01-01 21:39:25 +0000

    Load styles in the application using Webpack

--- 8.1 Course Project_Initialize/src/index.js
+++ 8.1 Course Project_Initialize/src/index.js
@@ -2,53 +2,56 @@ import $ from 'jquery';
 import _ from 'lodash';
 import bootstrap from 'bootstrap';
 import Typed from 'typed.js';
+import validate from 'validate.js';
+import toastr from 'toastr';
 
-import "./libs/toast/toastr.scss";
+import './_vendor.scss';
+import './index.css';
 
-const footerCol1Items = ["Home", "About", "Contact us", "FAQ", "Blog"];
-const footerCol2Items = ["Flutter", "Javascript", "Java", "Nodejs", "Python"];
-const footerCol3Items = ["Home", "Feature", "Pricing", "FAQ", "About"];
+const footerCol1Items = ['Home', 'About', 'Contact us', 'FAQ', 'Blog'];
+const footerCol2Items = ['Flutter', 'Javascript', 'Java', 'Nodejs', 'Python'];
+const footerCol3Items = ['Home', 'Feature', 'Pricing', 'FAQ', 'About'];
 
 //Animating the text
 
 var options = {
-  strings: ["<i>Top Educational Courses</i>"],
+  strings: ['<i>Top Educational Courses</i>'],
   typeSpeed: 40,
-  showCursor: false,
+  showCursor: false
 };
 
-var typed = new Typed(".typed", options);
+var typed = new Typed('.typed', options);
 
 //Validate the email id
 
-$("#subscribeBtn").on("click", (e) => {
+$('#subscribeBtn').on('click', (e) => {
   e.preventDefault();
-  const emailId = $("#newsletter1")[0].value;
+  const emailId = $('#newsletter1')[0].value;
   const isInvalid = validate.single(emailId, { presence: true, email: true });
   if (isInvalid === undefined) {
     console.log();
-    toastr.success("You have been subscribed !");
+    toastr.success('You have been subscribed !');
   } else {
-    toastr.error("Invalid email address is entered!");
+    toastr.error('Invalid email address is entered!');
   }
 });
 
 // Build footer items
 function buildFooterItems(footerCol, itemsArr) {
-  let listItems = "";
+  let listItems = '';
   _.forEach(itemsArr, (item) => {
     let menuItem =
       '<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">' +
       item +
-      "</a></li>";
+      '</a></li>';
     listItems += menuItem;
   });
   footerCol.innerHTML = listItems;
 }
 
-const footerCol1 = $("#footer-column-1")[0];
-const footerCol2 = $("#footer-column-2")[0];
-const footerCol3 = $("#footer-column-3")[0];
+const footerCol1 = $('#footer-column-1')[0];
+const footerCol2 = $('#footer-column-2')[0];
+const footerCol3 = $('#footer-column-3')[0];
 
 buildFooterItems(footerCol1, footerCol1Items);
 buildFooterItems(footerCol2, footerCol2Items);
