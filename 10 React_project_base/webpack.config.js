02657d7306251be50dd490e5e61c6662751408bb
CRAMINH <catia.raminhos@daimler.com>
2024-02-18 22:16:48 +0000

    Add loaders and dev server configurations

--- 10 React_project_base/webpack.config.js
+++ 10 React_project_base/webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     filename: '[name].bundle.js',
@@ -13,5 +15,28 @@ module.exports = {
       template: './src/index.html',
       filename: 'index.html'
     })
-  ]
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+      port: 9000,
+      open: true
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCSSExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [MiniCSSExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(jpg|png|jpeg|gif)$/,
+        type: 'asset/resource'
+      }
+    ]
+  }
 };
